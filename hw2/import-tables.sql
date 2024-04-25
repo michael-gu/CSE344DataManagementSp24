695cf4fd642591ad8bb2c9aa8ba30a55730ab43d
Michael Gu <michgu@cs.washington.edu>
2024-04-25 16:50:53 -0700

    repository refactoring

--- hw2/import-tables.sql
+++
@@ -1,5 +0,0 @@
-.mode csv
-.import carriers.csv CARRIERS
-.import months.csv MONTHS
-.import weekdays.csv WEEKDAYS
-.import flights-small.csv FLIGHTS
---
+++ import-tables.sql
@@ -0,0 +1,5 @@
+.mode csv
+.import ../carriers.csv CARRIERS
+.import ../months.csv MONTHS
+.import ../weekdays.csv WEEKDAYS
+.import ../flights-small.csv FLIGHTS
